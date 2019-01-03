44ed75ef0a337f8cddecb3cec12a19b6aa3b9411
Jessy Hu <jesshuy12@gmail.com>
2019-01-03 06:17:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+  var dwarfString = ""
+  var num = 0
+	for (var i = 0; i < dwarves.length; i++) {
+	  num++
+		dwarfString = dwarfString + num + ". " + dwarves[i]
+		if (i != dwarves.length) {
+			dwarfString = dwarfString + " "
+		}
+	}
+	return dwarfString
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(planeteerCalls => planeteerCalls.toUpperCase()+"!")
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if(words[i].length > 4) {
+      return true
+    } else {
+      return false
+  }
+ }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+    return foods[i]
+  }
+  }
+  return "no cheese!"
 }
