63e00b37efe568c0a5b529aaef0e128055b7adc0
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 23:32:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ function dwarfRollCall(dwarves) {
 	return dwarfString
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
-  return planeteerCalls.map(planeteerCalls => planeteerCalls.toUpperCase()+"!")
+  return planeteerCalls.map(planeteerCalls.toUpperCase()+"!")
 }
 
 function longPlaneteerCalls(words) {
   for (var i = 0; i < words.length; i++) {
     if(words[i].length > 4) {
       return true
-    } else {
+    } 
       return false
   }
  }
-}
+
 
 function findTheCheese (foods) {
   for (var i = 0; i < foods.length; i++) {
